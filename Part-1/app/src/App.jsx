fde0db1a45665364e58a0e521c4e66d0962b8f02
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-30 18:00:40 -0400

    Update App - use multiple components

--- Part-1/app/src/App.jsx
+++ Part-1/app/src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
 
-const App = () => {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-
+const Hello = () => {
   return (
     <div>
-      <p>Hello World, it is now {now.toString()}</p>
-      <p>{a} plus {b} is {a+b}</p>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   )
 }
