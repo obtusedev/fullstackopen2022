1952150bc466db62156923f2ab2eb7d953a9c771
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-30 18:10:44 -0400

    Part 1: Update App - use props

--- Part-1/app/src/App.jsx
+++ Part-1/app/src/App.jsx
@@ -1,22 +1,26 @@
 import "./App.css";
 
-const Hello = () => {
-  return (
-    <div>
-      <p>Hello World</p>
-    </div>
-  )
-}
+const Hello = props => {
+    return (
+        <div>
+            <p>Hello {props.name}, you are {props.age} years old</p>
+        </div>
+    );
+};
 
 const App = () => {
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
-  )
-}
+    const name = "Peter";
+    const age = 10;
+
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name="Alice" />
+            <Hello name="Bob" />
+            <Hello name="foo" />
+            <Hello name={name} age={age} />
+        </div>
+    );
+};
 
 export default App;
