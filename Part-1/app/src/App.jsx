7afd702b3319afabe0724716fbd0f717be251082
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-30 22:06:33 -0400

    Part 1: Update App - guess birth year

--- Part-1/app/src/App.jsx
+++ Part-1/app/src/App.jsx
@@ -1,9 +1,14 @@
 const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  }
   return (
     <div>
       <p>
         Hello {props.name}, you are {props.age} years old
       </p>
+      <p>Your birth year is probably {bornYear()}</p>
     </div>
   )
 }
