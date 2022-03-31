c1db2d29d0227a0d8c8942476e1d194df9e50316
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-30 22:03:11 -0400

    Part 1: Update App - part b

--- Part-1/app/src/App.jsx
+++ Part-1/app/src/App.jsx
@@ -1,69 +1,24 @@
-const Header = props => {
-    return (
-        <>
-            <h1>{props.course}</h1>
-        </>
-    );
-};
-
-const Part = props => {
-    return (
-        <p>
-            {props.part} {props.exercise}
-        </p>
-    );
-};
-
-const Content = props => {
-    let { name: part1, exercises: exercises1 } = props.parts[0];
-    let { name: part2, exercises: exercises2 } = props.parts[1];
-    let { name: part3, exercises: exercises3 } = props.parts[2];
-    return (
-        <>
-            <Part part={part1} exercise={exercises1} />
-            <Part part={part2} exercise={exercises2} />
-            <Part part={part3} exercise={exercises3} />
-        </>
-    );
-};
-
-const Total = props => {
-    let first = props.parts[0].exercises;
-    let second = props.parts[1].exercises;
-    let third = props.parts[2].exercises;
-    return (
-        <>
-            <p>Number of exercises {first + second + third}</p>
-        </>
-    );
-};
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
 
 const App = () => {
-    const course = {
-        name: "Half Stack application development",
-        parts: [
-            {
-                name: "Fundamentals of React",
-                exercises: 10,
-            },
-            {
-                name: "Using props to pass data",
-                exercises: 7,
-            },
-            {
-                name: "State of a component",
-                exercises: 14,
-            },
-        ],
-    };
-    let {name, parts} = course;
-    return (
-        <div>
-            <Header course={name} />
-            <Content parts={parts} />
-            <Total parts={parts} />
-        </div>
-    );
-};
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
 
 export default App;
