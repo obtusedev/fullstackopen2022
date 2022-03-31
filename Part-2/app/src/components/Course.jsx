f855af51c0e348a7f49c7edadd5595dc3d08071d
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-31 16:21:53 -0400

    Part 2: Update App - exercise 2.2
    Show the total number of exercises.

--- Part-2/app/src/components/Course.jsx
+++ Part-2/app/src/components/Course.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 const Course = ({ course }) => {
+    // the 0 sets the inital value of total to 0.
+    let totalCourse = course.parts.reduce(
+        (total, part) => total + part.exercises,
+        0
+    );
     return (
         <div>
             <h1>{course.name}</h1>
@@ -9,6 +14,9 @@ const Course = ({ course }) => {
                     {part.name} {part.exercises}
                 </p>
             ))}
+            <p>
+                <b>total of {totalCourse} exercises</b>
+            </p>
         </div>
     );
 };
