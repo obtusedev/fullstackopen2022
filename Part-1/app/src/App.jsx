c437e5056c47d8acd6730812fde248dba4f17665
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-31 11:48:50 -0400

    Part 1: Update App - exercise 1.6 step1

--- Part-1/app/src/App.jsx
+++ Part-1/app/src/App.jsx
@@ -1,26 +1,16 @@
-import { useState } from "react";
-
-const Display = ({ counter }) => <div>{counter}</div>;
-
-const Button = ({ text, onClick }) => {
-    return <button onClick={onClick}>{text}</button>;
-};
+import { useState } from 'react'
 
 const App = () => {
-    const [counter, setCounter] = useState(0);
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-    const incrementCounter = () => setCounter(counter + 1);
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
 
-    const resetCounter = () => setCounter(0);
-
-    return (
-        <div>
-            <Display counter={counter} />
-
-            <Button text="plus" onClick={incrementCounter} />
-            <Button text="reset" onClick={resetCounter} />
-        </div>
-    );
-};
-
-export default App;
+export default App
