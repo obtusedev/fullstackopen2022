7e58f948f2ef2587df87c85dec46d2b62864c238
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-03-31 12:06:04 -0400

    Part 1: Update App - working prototype

--- Part-1/app/src/App.jsx
+++ Part-1/app/src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const Heading = ({ text }) => <h1>{text}</h1>;
+
+const Button = ({ text, onClick }) => {
+    return <button onClick={onClick}>{text}</button>;
+};
+
+const Tally = ({ good, neutral, bad }) => {
+    return (
+        <div>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+        </div>
+    );
+};
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
 
-  return (
-    <div>
-      code here
-    </div>
-  )
-}
+    const handleGood = () => setGood(good + 1);
+    const handleNeutral = () => setNeutral(neutral + 1);
+    const handleBad = () => setBad(bad + 1);
 
-export default App
+    return (
+        <div>
+            <Heading text="give feedback" />
+            <Button text="good" onClick={handleGood} />
+            <Button text="neutral" onClick={handleNeutral} />
+            <Button text="bad" onClick={handleBad} />
+            <Heading text="statistics" />
+            <Tally {...{ good, neutral, bad }} />
+        </div>
+    );
+};
+
+export default App;
