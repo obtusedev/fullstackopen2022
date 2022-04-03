9b9715337c246af04d152b74c4e8dcba9c3f287d
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-04-03 17:19:52 -0400

    Part 2: The Phonebook Step4 exercise 2.9
    Implement filtering by name.

--- Part-2/app-b/src/App.jsx
+++ Part-2/app-b/src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
 
+const dummyData = [
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+];
+
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [persons, setPersons] = useState(dummyData);
     const [newName, setNewName] = useState("Jane Doe");
+    const [newNumber, setNewNumber] = useState("123 456-7890");
+    // for users that match input filter
+    const [match, setMatch] = useState([]);
 
     const resetInput = e => {
-        setNewName("");
-        e.target.value = newName;
+        if (e.target.id === "name") {
+            setNewName("");
+            e.target.value = newName;
+        } else if (e.target.id === "number") {
+            setNewNumber("");
+            e.target.value = newNumber;
+        }
     };
 
-    const handlePersonChange = e => setNewName(e.target.value);
+    const handleNameChange = e => setNewName(e.target.value);
     const doesPersonExist = name =>
         persons.find(person => person.name === name);
 
@@ -17,26 +32,76 @@ const App = () => {
         e.preventDefault();
         let person = {
             name: newName,
+            number: newNumber,
         };
-        if (person.name === "") return;
+        if (person.name === "" || person.number === "") return;
         if (doesPersonExist(person.name) !== undefined) {
             // run if the user already exists.
             alert(`${person.name} is already added to phonebook`);
             return;
         }
         setPersons(persons.concat(person));
-        setNewName("");
+        setNewName(""); // reset name input
+        setNewNumber(""); // reset number input
+    };
+
+    const handleNumberChange = e => setNewNumber(e.target.value);
+
+    const handleFilterInputChange = e => {
+        // reset the matched names so you don't end up with duplicates
+        setMatch([]);
+        // if filter input is set to empty the just return all persons
+        if (e.target.value === "") return displayNumbers;
+        persons.filter(person => {
+            if (
+                person.name.toLowerCase().includes(e.target.value.toLowerCase())
+            ) {
+                // if person already exist in match do nothing
+                if (match.includes(person)) return;
+                setMatch(match.concat(person));
+            }
+        });
+    };
+
+    const displayNumbers = () => {
+        if (match.length === 0) {
+            return persons.map(person => (
+                <p key={person.name}>
+                    {person.name} - {person.number}
+                </p>
+            ));
+        } else {
+            return match.map(person => (
+                <p key={person.name}>
+                    {person.name} - {person.number}
+                </p>
+            ));
+        }
     };
 
     return (
         <div>
             <h2>Phonebook</h2>
+            filter show with
+            <input type="text" onChange={handleFilterInputChange} />
+            <h3>add a new</h3>
             <form onSubmit={addPerson}>
                 <div>
-                    name:{" "}
+                    name:
                     <input
+                        id="name"
+                        type="text"
                         value={newName}
-                        onChange={handlePersonChange}
+                        onChange={handleNameChange}
+                        onClick={resetInput}
+                    />
+                    <br />
+                    number:
+                    <input
+                        id="number"
+                        type="text"
+                        value={newNumber}
+                        onChange={handleNumberChange}
                         onClick={resetInput}
                     />
                 </div>
@@ -45,9 +110,7 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(person => (
-                <p key={person.name}>{person.name}</p>
-            ))}
+            {displayNumbers()}
         </div>
     );
 };
