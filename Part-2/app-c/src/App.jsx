6c80c2acf4e5927dd1bf0985111f7f56f59a3a56
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-04-13 18:36:54 -0400

    Part 2: Add toggle more details exercise 2.13

--- Part-2/app-c/src/App.jsx
+++ Part-2/app-c/src/App.jsx
@@ -2,8 +2,26 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
+function Country(country) {
+    return (
+        <div key={country.name.common}>
+            <h1>{country.name.common}</h1>
+            <p>capital {country.capital}</p>
+            <p>area {country.area}</p>
+            <h3>languages:</h3>
+            <ul>
+                {Object.values(country.languages).map(lang => (
+                    <li key={lang}>{lang}</li>
+                ))}
+            </ul>
+            <img src={country.flags.png} alt={`${country.name.common} flag`} />
+        </div>
+    );
+}
+
 function App() {
     const [countries, setCountries] = useState([]);
+    const [showDetails, setShowDetails] = useState({ show: false, name: "" });
 
     const fetchCountries = async name => {
         let res = await axios.get("https://restcountries.com/v3.1/all");
@@ -15,40 +33,46 @@ function App() {
                 matched.push(country);
             }
         });
+        // sort alphabetically
+        matched.sort((a, b) => (a.name.common > b.name.common ? 1 : -1));
         setCountries(matched);
+    };
+
+    const showMore = () => {
+        let country = countries.find(c => c.name.common === showDetails.name);
+        if (showDetails.show) {
+            return <Country {...country} />;
+        }
     };
 
     const handleInput = e => {
         fetchCountries(e.target.value);
     };
 
+    const handleClick = e => {
+        setShowDetails(prevState => ({
+            ...prevState,
+            show: !prevState.show,
+            name: e.target.id,
+        }));
+    };
+
     const display = () => {
         if (countries.length > 10) {
             return <p>Too many matches, specify another filter</p>;
         } else if (countries.length > 1 && countries.length <= 10) {
-            return countries.map(country => (
-                <p key={country.name.official}>{country.name.common}</p>
-            ));
-        } else if (countries.length === 1) {
             return countries.map(country => {
                 return (
-                    <div key={country.name.common}>
-                        <h1>{country.name.common}</h1>
-                        <p>capital {country.capital}</p>
-                        <p>area {country.area}</p>
-                        <h3>languages:</h3>
-                        <ul>
-                            {Object.values(country.languages).map(lang => (
-                                <li key={lang}>{lang}</li>
-                            ))}
-                        </ul>
-                        <img
-                            src={country.flags.png}
-                            alt={`${country.name.common} flag`}
-                        />
+                    <div key={country.name.official}>
+                        <span>{country.name.common}</span>
+                        <button id={country.name.common} onClick={handleClick}>
+                            show
+                        </button>
                     </div>
                 );
             });
+        } else if (countries.length === 1) {
+            return <Country {...countries[0]} />;
         }
     };
 
@@ -56,6 +80,7 @@ function App() {
         <div>
             find countries <input onChange={handleInput} />
             <div>{display()}</div>
+            <div>{showMore()}</div>
         </div>
     );
 }
