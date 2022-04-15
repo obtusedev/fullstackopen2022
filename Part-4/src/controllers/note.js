c01a4d346ea28bef0bbfcdf123ddb2553bd8e151
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-04-15 16:44:33 -0400

    Part 4: Add note controllers

---
+++ Part-4/src/controllers/note.js
@@ -0,0 +1,49 @@
+const notesRouter = require("express").Router();
+const Note = require("../models/Note");
+
+notesRouter.get("/", async (req, res) => {
+    let notes = await Note.find({}, "-_id -__v");
+    res.status(200).json(notes);
+});
+
+notesRouter.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    let note = await Note.findOne({ id: id }, "-_id title content important");
+    return note
+        ? res.status(200).json(note)
+        : res.status(404).json({ error: "note not found" });
+});
+
+notesRouter.post("/", async (req, res) => {
+    const body = req.body;
+    if (!body.title || !body.content) {
+        console.log(req.body);
+        return res.status(400).json({ error: "missing required fields" });
+    }
+    const newNote = {
+        id: body.id,
+        title: body.title,
+        content: body.content,
+        important: body.important || false,
+    };
+    try {
+        let noteCreated = await Note.create(newNote);
+        res.status(200).json({ message: "note created" });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+notesRouter.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        let result = await Note.deleteOne({ id: id });
+        if (result.deletedCount !== 0) {
+            res.status(200).json({ message: "note deleted successfully" });
+        }
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+module.exports = notesRouter;
